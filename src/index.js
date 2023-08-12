5163afb23e40c986a2e2f024f9cb09725a3e8cfc
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-08-12 14:56:33 +0300

    my five

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from "axios";
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import './css/index.css';
 
 const loadMore = document.querySelector(".load-more");
 loadMore.style.display = "none";
@@ -11,57 +12,74 @@ const myForm = document.querySelector('#search-form');
 
 let query;
 let page = 1;
-let per_page = 3;
+let per_page = 40;
 
-// let simpleGallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
-var lightbox = new SimpleLightbox('.gallery a');
+let lightbox = new SimpleLightbox('.photo-card a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
 const getRequest = async (e) => {
     const response = await axios.get(`https://pixabay.com/api/?key=38733700-96318c553c84bf90463eb3752&q=${e}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`);
     const data = await response.data;
     const hits = await data.hits;
-    if (hits.length) { return hits }
-    else Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    const totalImg = await data.totalHits;
+
+
+    if (!hits.length) {
+        loadMore.style.display = "none";
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        return;
+    } else {
+        loadMore.style.display = "block";
+        renderGallery(hits);
+        page += 1;
+        totalImages(totalImg);
+    }
 };
 
-function onSubmit2(e) {
+function totalImages(totalImg) {
+    let availablePage = totalImg / 40;
+    if (page > availablePage) {
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        loadMore.style.display = "none";
+    }
+};
+
+function onSubmit(e) {
     e.preventDefault();
     if (e.type === 'submit') {
         page = 1;
         gallery.innerHTML = '';
         query = e.target.elements.searchQuery.value
     }
-    getRequest(query).then(hits => hits.map(hit => {
-        // gallery.innerHTML += `
-        // <div class="photo-card">
-        //     <img src=${hit.webformatURL} alt=${hit.tags} loading="lazy" />
-        //     <div class="info">
-        //         <p class="info-item">
-        //             <b>Likes ${hit.likes}</b>
-        //         </p>
-        //         <p class="info-item">
-        //             <b>Views ${hit.views}</b>
-        //         </p>
-        //         <p class="info-item">
-        //             <b>Comments ${hit.comments}</b>
-        //         </p>
-        //         <p class="info-item">
-        //             <b>Downloads ${hit.downloads}</b>
-        //         </p>
-        //     </div>
-        // </div>`
-        gallery.innerHTML += `<a href="${hit.webformatURL}"><img src="${hit.previewURL}")></a>`;
-        // alt = "${hit.tags}"
-
-    }).join(""))
-        .then(page += 1)
-        .then(lightbox.refresh())
-        .then(loadMore.style.display = "block")
-        .catch(error => { console.log("Sorry, there are no images matching your search query. Please try again.") });
+    getRequest(query).catch(error => { console.log("Sorry, there are no images matching your search query. Please try again.") });
 };
 
+function renderGallery(hits) {
+    hits.map(hit => {
+        gallery.innerHTML +=
+            `<div class="photo-card">
+    <div class="photo-block">
+        <a href="${hit.largeImageURL}">
+            <img class="photo-img" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+        </a>
+    </div>
+    <div class="info">
+      <p class="info-item"><b>Likes</b>${hit.likes}</p>
+      <p class="info-item"><b>Views</b>${hit.views}</p>
+      <p class="info-item"><b>Comments</b>${hit.comments}</p>
+      <p class="info-item"><b>Downloads</b>${hit.downloads}</p>
+    </div>
+</div>`;
+    });
+    lightbox.refresh();
+}
 
-myForm.addEventListener('submit', onSubmit2);
-loadMore.addEventListener('click', onSubmit2);
+myForm.addEventListener('submit', onSubmit);
+loadMore.addEventListener('click', onSubmit);
+
+
 
 
 
